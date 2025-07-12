8d019412fef42eeaa658e1e587748f6ec51f2683
ryo-ya <sax_ry_rp@outlook.jp>
2025-07-12 20:28:50 +0900

    2025/07/12 画面修正

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,8 @@
 module HomeHelper
+    def get_age(birthday)
+        now = Date.today
+        age = now.year - birthday.year
+        age -= 1 if birthday > now.years_ago(age)
+        age
+    end
 end
